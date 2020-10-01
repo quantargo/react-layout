545f28564b96ece56b3c427156101983d8786b0e
Christian Kaindl <christian.kaindl@quantargo.com>
2020-10-01 14:47:13 +0200

    LayoutFlex - Fixes and TypeScript tweaks

--- src/LayoutFlex.tsx
+++ src/LayoutFlex.tsx
@@ -1,37 +1,30 @@
 import React from 'react'
-import * as CSS from 'csstype'
 
 // interface CustomComponent<Type> {
 //   (...props: Props): React.ReactType<Type>;
 // }
 
-export enum flexDirection {
-  'row',
-  'row-reverse',
-  'column',
-  'column-reverse'
-}
-export interface Props {
+export type Props = {
   // as?: HTMLElementTagNameMap | CustomComponent<React.ReactType>,
-  as?: any;
-  direction?: flexDirection;
-  alignItems?: CSS.Property.AlignItems;
-  justifyContent?: CSS.Property.JustifyContent;
-  wrap?: CSS.Property.FlexWrap | boolean;
-  bla?: number;
-  style?: {
-    [key: string]: any
-  };
+  /**
+   * Render as custom component
+   */
+  as?: any,
+  flexDirection?: 'row' | 'column' | 'column-reverse' | 'row-reverse',
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch',
+  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'space-around' | 'space-between' | 'space-evenly',
+  flexWrap?: 'wrap' | 'wrap-reverse' | 'nowrap' | boolean,
+  style?: React.CSSProperties
 }
 
 type Ref = HTMLElement
 
 export const LayoutFlex = React.forwardRef<Ref, Props>(function LayoutFlex ({
   as: Tag = 'div',
-  direction = 'row',
+  flexDirection = 'row',
   alignItems = 'stretch',
   justifyContent = 'space-between',
-  wrap = 'no-wrap',
+  flexWrap = 'nowrap',
   children,
   style,
   ...props
@@ -42,10 +35,10 @@ export const LayoutFlex = React.forwardRef<Ref, Props>(function LayoutFlex ({
       ref={ref}
       style={{
         display: 'flex',
-        flexDirection: direction,
+        flexDirection,
         alignItems,
         justifyContent,
-        flexWrap: wrap === true ? 'wrap' : wrap,
+        flexWrap: flexWrap === true ? 'wrap' : flexWrap,
         ...style
       }}
     >

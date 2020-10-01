3eedabd72f3f1255450c84ce10c5b69db931692a
Christian Kaindl <christian.kaindl@quantargo.com>
2020-10-01 19:30:44 +0200

    Add support CSS Modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,20 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async function enableCssModules (config) {
+    // Make whatever fine-grained changes you need
+
+    // Rule 8 (seventh index) has a CSS loader.
+    // Exclude .modeul.css files to prevent clashing with it (id doesn't work) 
+    config.module.rules[7].exclude = /\.module.css$/
+
+    // Add CSS Modules loader
+    config.module.rules.push({
+      test: /\.module.css$/,
+      use: ['style-loader', 'css-loader']
+    })
+
+    return config
+  }
 }

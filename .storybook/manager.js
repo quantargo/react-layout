f7428003771c97ace902c2edf77cff9efeb8d3e0
Christian Kaindl <christian.kaindl@quantargo.com>
2020-10-01 15:35:18 +0200

    Branding - Add Quantargo logo

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons'
+import theme from './quantargoTheme'
+
+addons.setConfig({
+  theme,
+})
---
+++ .storybook/quantargoTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+import logo from '../src/assets/logo.svg'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Quantargo',
+  brandUrl: 'https://quantargo.com',
+  brandImage: logo,
+})
